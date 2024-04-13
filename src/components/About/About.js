9675ef4ed6053f1d0eb0c52d6fc87f7d26fc77a5
studbaneso1322 <soumyadip.banerjee@stud.th-rosenheim.de>
2024-04-14 01:05:20 +0200

    initial commit

---
+++ src/components/About/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Soumyadip</strong>. I'm originally from India and have been living in Germany for the past three years. During my time here, I gained valuable experience through various entry-level positions and internships, which greatly enhanced my programming experience, communication skills, and adaptability.
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            My journey into the world of technology began with a strong passion for web-developement, which led me to pursue the Applied Artificial intelligence program. Despite the hard work, I remained determined and forged lasting friendships that supported me throughout this demanding program.
+            Throughout my studies at TH Rosenheim, I had the opportunity to engage in fascinating projects. Notably, I contributed to the development of the Media Infrastructure at German Aerospace center, project that garnered recognition from my colleagues, instructors, and clients alike.
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and computer vision.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,120 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/renr-responsive.png",
+    title: "Faulty board detector",
+    description:
+      "A cloud-Based Platform to detect faulty wooden-boards in production via 1080p Camera",
+    tech_stack: ["OpenCV", "Azure", "Python"],
+    github_url: "",
+    demo_url: "",
+  },
+  {
+    img: "/mymind-responsive.png",
+    title: "Stay for ever",
+    description:
+      "A social media platform to share memories of your deceased ones.",
+    tech_stack: ["HTML", "TCSS", "Django", "MySQL", "Typescript","Javascript","Python","Azure"],
+    github_url: "",
+    demo_url: "https://Stay-for-ever.net/",
+  },
+  {
+    img: "/pokedex-responsive.png",
+    title: "LiCa fuse",
+    description:
+      "A Ongoing project to fuse Lidar sensor's point-cloud and camera footage for autonomous driving.",
+    tech_stack: ["Jetson nano", "Python", "C++"],
+    github_url: "",
+    demo_url: "",
+  },
+
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://github.com/devicons/devicon/blob/master/icons/opencv/opencv-original-wordmark.svg",
+    name: "OpenCV",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://github.com/devicons/devicon/blob/master/icons/django/django-plain.svg",
+    name: "Django",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://github.com/devicons/devicon/blob/master/icons/docker/docker-plain-wordmark.svg",
+    name: "Docker",
+  },
+  {
+    img: "https://github.com/devicons/devicon/blob/master/icons/spring/spring-original-wordmark.svg",
+    name: "Spring Boot",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://www.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-logo.svg",
+    name: "AWS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://github.com/devicons/devicon/blob/master/icons/cplusplus/cplusplus-original.svg",
+    name: "C++",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://gstreamer.freedesktop.org/data/images/artwork/gstreamer-logo.svg",
+    name: "Gstreamer",
+  },
+];
